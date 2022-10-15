8600756afd084b44bbcf484a7f9ea0ba137e39f8
shariq Ansari <ansari.shariq.badshah@gmail.com>
2022-10-15 15:25:25 +0530

    first commit

---
+++ src/app/store.js
@@ -0,0 +1,30 @@
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from '../features/counter/counterSlice'
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+// import thunk from 'redux-thunk';
+
+
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+  
+  const persistedReducer = persistReducer(persistConfig, counterReducer)
+  
+
+
+
+
+
+export const store = configureStore({
+  reducer: {
+    counter: persistedReducer,
+    // middleware: [thunk]
+
+
+  },
+  
+})
+
+export const persistor = persistStore(store)
